8a258e4e8d7c364aa2f7b84f72151ec3f1f5c323
Luiz Fernando Sanches <luiz.sanches@gmail.com>
2024-12-24 14:35:28 -0300

    Inclusao de helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,14 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        app()->bind('asset', function () {
-            return function ($path, $secure = null) {
-                // Retrieve the base path from the .env file
-                $basePath = env('ASSET_BASE_PATH', '');
-
-                // Prepend the base path and call the default asset() helper
-                return URL::asset($basePath . ltrim($path, '/'), $secure);
-            };
-        });
+        //
     }
 }
