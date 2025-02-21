fd577b1d2aef3c89ad3fca075b8870125a66e302
Luiz Fernando Sanches <luiz.sanches@gmail.com>
2025-02-20 22:03:52 -0300

    fix app_url

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,6 +6,7 @@ class MainController extends AppBaseController
 {
     public function main()
     {
+        dd('Logging is not working, and this message confirms it.');
         return $this->render([]);
     }
 }
